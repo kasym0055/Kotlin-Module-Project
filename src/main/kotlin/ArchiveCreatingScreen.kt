f826ac20fb9ea970197a4d9096854557e18a6ad8
Practicum <kasymhr@gmail.com>
2025-12-16 17:01:52 +0500

    commiting the project

--- src/main/kotlin/ArchiveCreatingScreen.kt
+++ src/main/kotlin/ArchiveCreatingScreen.kt
@@ -3,6 +3,12 @@ class ArchiveCreatingScreen {
         println("Создание и добавление Архива")
         println("Название архива: ")
         val archiveName=readln()
+        if (archiveName.isBlank()){
+            println("Название архива не может быть пустым")
+            println("Нажмите Enter чтобы попробовать снова")
+            readln()
+            return Screen.ArchiveCreating
+        }
         val newArchive=ArchiveData(archiveName)
         newArchive.title=archiveName
         newArcObj.addArchive(newArchive)
--- src/main/kotlin/NoteCreatingScreen.kt
+++ src/main/kotlin/NoteCreatingScreen.kt
@@ -1,10 +1,22 @@
 fun noteCreatingScreen(archive:ArchiveData):Screen{
     println("Название: ")
     val title = readln()
+    if (checkIsBlank(title,"Название")) return Screen.NoteCreating
     println("Текст:")
     val text = readln()
+    if (checkIsBlank(text,"Текст")) return Screen.NoteCreating
 
     archive.notes.add(NoteData(title,text))
     println(archive.notes)
     return Screen.OpenArchive
+}
+
+fun checkIsBlank(str: String,title: String): Boolean{
+    if (str.isBlank()){
+        println("$title не можеть быть пустым")
+        println("Нажмите Enter чтобы попробовать снова")
+        readln()
+        return true
+    }
+    return false
 }
